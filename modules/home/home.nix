3f27438a45b0bc8a3f04f467291a5aca4c6ab73e
notohh <notohh@protonmail.com>
2023-01-30 06:16:14 -0500

    forgot to remove pkgs

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -12,12 +12,6 @@ imports = [
     username = "notoh";
     homeDirectory = "/home/notoh";
     stateVersion = "23.05";
-    packages = [
-      pkgs.alsa-lib
-      pkgs.boost
-      pkgs.qt5.full
-      pkgs.openssl
-    ];
   };
 
 }
