a32e4d258d4800f17cb0d49ec21e537fb7b2556d
notohh <notohh@protonmail.com>
2023-01-30 11:33:18 -0500

    import modules

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -3,7 +3,14 @@
 {
 
 imports = [
-  ./gtk/default.nix
+  ./gtk/gtk.nix
+  ./wezterm/wezterm.nix
+  ./zathura/zathura.nix
+  ./nushell/nushell.nix
+  ./starship/starship.nix
+  ./dunst/dunst.nix
+  ./helix/helix.nix
+  ./git/git.nix
 ];
  systemd.user.startServices = "sd-switch";
  programs.home-manager.enable = true;
