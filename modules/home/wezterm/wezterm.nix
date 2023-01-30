a0c03ed10f81aa74228d6641dbd01304cfeea20e
notohh <notohh@protonmail.com>
2023-01-30 11:32:32 -0500

    feat: configure wezterm in nix

---
+++ modules/home/wezterm/wezterm.nix
@@ -0,0 +1,23 @@
+{default, ...}: {
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+     local wezterm = require 'wezterm'
+
+     return {
+       enable_wayland = false,
+       font = wezterm.font 'Comic Code Ligatures',
+       font_size = 12.0,
+       hide_tab_bar_if_only_one_tab = true,
+       color_scheme = 'Catppuccin Mocha',
+
+       window_padding = {
+        top = 0,
+        bottom = 0,
+        left = 0,
+        right = 0, 
+  }
+}
+'';
+   };
+}
