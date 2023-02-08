e3c7078d6d2719c05df24b5a61dbce911dda6efe
notohh <github@notohh.dev>
2023-02-07 20:05:55 -0500

    feat: add cursor support

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -3,7 +3,16 @@
   config,
   ...
 }: {
-  config.gtk = {
+
+  home.pointerCursor = {
+    package = pkgs.catppuccin-cursors;
+    name = "Catppuccin-Mocha-Pink";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
+   gtk = {
     enable = true;
     theme = {
       name = "Catppuccin-Mocha-Standard-Pink-Dark";
