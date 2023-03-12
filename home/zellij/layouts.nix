4578e6245acbed4b174b98d736524a975a82bc40
notohh <github@notohh.dev>
2023-03-11 19:28:15 -0500

    feat: init ssh layout

--- home/zellij/layouts.nix
+++ home/zellij/layouts.nix
@@ -2,7 +2,7 @@
   default,
   ...
 }: {
-  home.file.".config/zellij/mainlayout.kdl".text = ''
+  home.file.".config/zellij/dev.kdl".text = ''
   layout {
     tab name="main" {
         pane size=1 borderless=true {
@@ -39,5 +39,29 @@
     }
 }
 
+  '';
+
+  home.file.".config/zellij/ssh.kdl".text = ''
+  layout {
+    tab name="ssh" {
+        pane size=1 borderless=true {
+          plugin location="zellij:tab-bar"
+        }
+        pane split_direction="vertical" {
+          pane 
+          pane split_direction="horizontal" {
+            pane command="ssh" {
+              args "oh@100.117.178.88"
+            }
+            pane command="ssh" {
+              args "oh@100.103.228.56"
+            }
+        }
+    }
+        pane size=2 borderless=true {
+          plugin location="zellij:status-bar"
+        }
+    }
+}
   '';
 }
