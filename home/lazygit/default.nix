39c5f0e23b82a0e2fcfdfe5ebc9cdfdd672aa875
notohh <github@notohh.dev>
2023-03-31 21:05:57 -0400

    feat: init lazygit theme

--- home/lazygit/default.nix
+++ home/lazygit/default.nix
@@ -5,6 +5,17 @@
     enable = true;
     settings = {
       windowSize = "normal";
+      gui.theme = {
+       lightTheme = false;
+       activeBorderColor = [ "#a6d189" "bold" ];
+       inactiveBorderColor = [ "#c6d0f5" ];
+       optionsTextColor = [ "#8caae" ];
+       selectedLineBgColor = [ "#414559" ];
+       selectedRangeByColor = [ "414559" ];
+       cherryPickedCommitBgColor =  [ "81c8be" ];
+       cherryPickedCommitFgColor = [ "#8caaee" ];
+       unstagedChangesColor = [ "red" ];
+      };
     };
   };
 }
