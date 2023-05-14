c9db8b1b8abc821bb80fc5964dcec3e6befea85a
notohh <github@notohh.dev>
2023-05-14 05:56:42 -0400

    fix: foundry sops

--- modules/services/foundryvtt.nix
+++ modules/services/foundryvtt.nix
@@ -1,13 +1,13 @@
-{inputs, ...}: {
+{config, ...}: {
   virtualisation.oci-containers.containers.foundryvtt = {
     image = "felddy/foundryvtt:release";
     ports = ["30000:30000"];
     volumes = [
       "/home/notoh/docker/foundryvtt:/data"
     ];
-    environment = {
-      FOUNDRY_USERNAME = inputs.sops.secrets.foundry-username;
-      FOUNDRY_PASSWORD = inputs.sops.secrets.foundry-password;
-    };
+    environmentFiles = [
+      /run/secrets/foundry-username
+      /run/secrets/foundry-password
+    ];
   };
 }
