1b7aafd9a9fd6a7cd6198dd72a86fa74c9f3dfac
notohh <github@notohh.dev>
2023-05-14 01:48:33 -0400

    foundry: use sops

--- modules/services/foundryvtt.nix
+++ modules/services/foundryvtt.nix
@@ -1,4 +1,4 @@
-{...}: {
+{inputs, ...}: {
   virtualisation.oci-containers.containers.foundryvtt = {
     image = "felddy/foundryvtt:release";
     ports = ["30000:30000"];
@@ -6,8 +6,8 @@
       "/home/notoh/docker/foundryvtt:/data"
     ];
     environment = {
-      FOUNDRY_USERNAME = "";
-      FOUNDRY_PASSWORD = "";
+      FOUNDRY_USERNAME = inputs.sops.secrets.foundry-username;
+      FOUNDRY_PASSWORD = inputs.sops.secrets.foundry-password;
     };
   };
 }
