53f849e8b7637328bce7e7f44df8d20e03972556
notohh <github@notohh.dev>
2023-05-14 23:03:59 -0400

    feat: init ssh config in home manager

--- home/default.nix
+++ home/default.nix
@@ -16,4 +16,28 @@
     lazydocker
     yt-dlp
   ];
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host hime
+        HostName 192.168.1.66
+        User oh
+        IdentityFile /home/notoh/.ssh/hime
+      Host sutakku
+        HostName 192.168.1.72
+        User oh
+        IdentityFile /home/notoh/.ssh/sutakku
+      Host sakura
+        HostName 192.168.1.25
+        User notoh
+        IdentityFile /home/notoh/.ssh/sakura
+      Host pihole
+        Hostname 192.168.1.221
+        User root
+      Host pve1
+        Hostname 192.168.1.36
+        User root
+    '';
+  };
 }
