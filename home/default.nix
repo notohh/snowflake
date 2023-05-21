32ceb1262c4cf117f50d1e5c562c808e3f65274e
notohh <github@notohh.dev>
2023-05-21 16:49:26 -0400

    home: init gpg agent, move bat

--- home/default.nix
+++ home/default.nix
@@ -9,6 +9,7 @@
     ./lf
     ./direnv
     ./btop
+    ./bat
   ];
 
   home.packages = with pkgs; [
@@ -18,8 +19,14 @@
     lazydocker
     yt-dlp
     nfs-utils
-    pinentry-curses
   ];
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 3600;
+    pinentryFlavor = "gtk2";
+    enableSshSupport = true;
+  };
 
   programs.ssh = {
     enable = true;
@@ -41,23 +48,7 @@
     '';
   };
 
-  programs.bat = {
-    enable = true;
-    config = {
-      theme = "catppuccin-mocha";
-    };
-    themes = {
-      catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        }
-        + "/Catppuccin-mocha.tmTheme");
-    };
-  };
-
-  programs.zoxide = {
+   programs.zoxide = {
     enable = true;
     enableNushellIntegration = true;
     options = [
