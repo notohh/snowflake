8a44463847c1067fdfbae6ee5705ee59e1392aff
notohh <github@notohh.dev>
2023-06-06 00:09:09 -0400

    forgejo: enable actions

--- hosts/sakura/services/forgejo.nix
+++ hosts/sakura/services/forgejo.nix
@@ -5,6 +5,10 @@
     appName = "forgejo";
     stateDir = "/sakura/forgejo";
     settings = {
+      actions = {
+        ENABLED = true;
+        DEFAULT_ACTIONS_URL = "https://gitea.com";
+      };
       service.DISABLE_REGISTRATION = true;
       server = {
         HTTP_PORT = 3000;
