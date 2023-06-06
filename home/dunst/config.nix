6a73e204d72baf4a545becd441d4b106dac1de62
notohh <github@notohh.dev>
2023-06-05 23:57:46 -0400

    dunst: offset adjust, switch font

--- home/dunst/config.nix
+++ home/dunst/config.nix
@@ -7,8 +7,9 @@
     width = "300";
     min_icon_size = 32;
     max_icon_size = 128;
-    offset = "0";
-    font = "JetBrainsMono Nerd Font 10";
+    origin = "top-right";
+    offset = "14x61";
+    font = "jost 10";
   };
   urgency_low = {
     background = "#1E1E2E";
