1691fe65d16999b403075031961472b53b31db26
notohh <github@notohh.dev>
2023-06-11 00:42:13 -0400

    ame: switch to powersave

--- hosts/ame/hardware-configuration.nix
+++ hosts/ame/hardware-configuration.nix
@@ -35,6 +35,6 @@
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
