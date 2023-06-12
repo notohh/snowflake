51eaa177b66626b8288a2bc4111f7fc8d3816313
notohh <github@notohh.dev>
2023-06-12 00:46:43 -0400

    traefik: remove prod config

--- hosts/yuki/services/traefik.nix
+++ hosts/yuki/services/traefik.nix
@@ -1,14 +1,4 @@
-{config, ...}: {
-  sops.secrets.cloudflare-api-key = {};
-  networking.firewall.allowedTCPPorts = [80 443];
-  systemd.services.traefik = {
-    environment = {
-      CLOUDFLARE_EMAIL = "jch0tm2e@notohh.dev";
-    };
-    serviceConfig = {
-      EnvironmentFile = [config.sops.secrets.cloudflare-api-key.path];
-    };
-  };
+_: {
   services.traefik = {
     enable = true;
     dynamicConfigOptions = {
@@ -41,28 +31,6 @@
       entryPoints = {
         websecure.address = ":443";
         web.address = ":80";
-      };
-      certificatesResolvers = {
-        staging.acme = {
-          email = "x3xr6n66@notohh.dev";
-          storage = "/var/lib/traefik/acme.json";
-          caServer = "https://acme-staging-v02.api.letsencrypt.org/directory";
-          dnsChallenge = {
-            provider = "cloudflare";
-            resolvers = ["1.1.1.1:53" "1.0.0.1:53"];
-            delayBeforeCheck = "0";
-          };
-        };
-        production.acme = {
-          email = "x3xr6n66@notohh.dev";
-          storage = "/var/lib/traefik/acme.json";
-          caServer = "https://acme-v02.api.letsencrypt.org/directory";
-          dnsChallenge = {
-            provider = "cloudflare";
-            resolvers = ["1.1.1.1:53" "1.0.0.1:53"];
-            delayBeforeCheck = "0";
-          };
-        };
       };
     };
   };
