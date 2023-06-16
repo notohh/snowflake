eb48ae6a40d77a3e9754249473af2e1336dcce07
notohh <github@notohh.dev>
2023-06-16 04:06:29 -0400

    users: add uid

--- modules/users.nix
+++ modules/users.nix
@@ -3,6 +3,7 @@
     defaultUserShell = pkgs.nushell;
     users.notoh = {
       isNormalUser = true;
+      uid = 1000;
       description = "notoh";
       extraGroups = [
         "networkmanager"
