afe37c744695165c8f3b5124a1a2277bb8147362
notohh <github@notohh.dev>
2023-06-17 12:56:27 -0400

    woodpecker: make it functional

--- hosts/sakura/services/woodpecker.nix
+++ hosts/sakura/services/woodpecker.nix
@@ -5,11 +5,16 @@
     enable = true;
     environment = {
       WOODPECKER_SERVER_ADDR = ":8200";
+      WOODPECKER_GRPC_ADDR = ":8300";
       WOODPECKER_HOST = "https://ci.notohh.dev";
       WOODPECKER_OPEN = "false";
       WOODPECKER_GITEA = "true";
       WOODPECKER_GITEA_URL = "https://git.notohh.dev";
       WOODPECKER_ADMIN = "notohh";
+      WOODPECKER_AGENT_SECRET = config.sops.secrets.woodpecker-agent-secret.path;
+      WOODPECKER_LOG_LEVEL = "debug";
+      WOODPECKER_DEBUG_PRETTY = "true";
+      WOODPECKER_KEEPALIVE_MIN_TIME = "10s";
     };
     environmentFile = config.sops.secrets.woodpecker-server.path;
   };
@@ -19,8 +24,12 @@
     environment = {
       DOCKER_HOST = "unix:///var/run/docker.sock";
       WOODPECKER_BACKEND = "docker";
-      WOODPECKER_SERVER = "localhost:8200";
+      WOODPECKER_SERVER = "localhost:8300";
       WOODPECKER_AGENT_SECRET = config.sops.secrets.woodpecker-agent-secret.path;
+      WOODPECKER_LOG_LEVEL = "debug";
+      WOODPECKER_DEBUG_PRETTY = "true";
+      WOODPECKER_KEEPALIVE_MIN_TIME = "10s";
+      WOODPECKER_HEALTHCHECK = "true";
     };
     extraGroups = [
       "docker"
