fc5acf594f642ef66d92a165274002c0b37bb5f8
notohh <github@notohh.dev>
2023-06-24 19:11:16 -0400

    sora: init traefik

--- hosts/sora/services/default.nix
+++ hosts/sora/services/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./traefik.nix
     ./uptimekuma.nix
   ];
 }
---
+++ hosts/sora/services/traefik.nix
@@ -0,0 +1,86 @@
+{config, ...}: {
+  sops.secrets.cloudflare-api-key = {};
+  networking.firewall.allowedTCPPorts = [80 443];
+  systemd.services.traefik = {
+    environment = {
+      CLOUDFLARE_EMAIL = "jch0tm2e@notohh.dev";
+    };
+    serviceConfig = {
+      EnvironmentFile = [config.sops.secrets.cloudflare-api-key.path];
+    };
+  };
+  services.traefik = {
+    enable = true;
+    dynamicConfigOptions = {
+      http = {
+        middlewares.authelia = {
+          forwardauth = {
+            address = "http://100.121.201.47:9091/api/verify?rd=https://passport.notohh.dev/";
+            trustForwardHeader = true;
+          };
+        };
+        routers = {
+          api = {
+            rule = "PathPrefix(`/api/`)";
+            entrypoints = ["websecure"];
+            service = "api@internal";
+          };
+          uptime-kuma = {
+            rule = "Host(`status.notohh.dev`)";
+            entrypoints = ["websecure"];
+            service = "uptime-kuma";
+            tls.domains = [{main = "*.notohh.dev";}];
+            tls.certresolver = "production";
+          };
+        };
+        services = {
+          uptime-kuma.loadBalancer.servers = [{url = "http://100.87.54.48:4000";}];
+        };
+      };
+    };
+    staticConfigOptions = {
+      log.level = "DEBUG";
+      api.dashboard = false;
+      api.insecure = false;
+      global = {
+        checkNewVersion = false;
+        sendAnonymousUsage = false;
+      };
+      entryPoints = {
+        websecure = {
+          address = ":443";
+        };
+        web = {
+          address = ":80";
+        };
+      };
+      metrics = {
+        prometheus = {
+          addServicesLabels = true;
+        };
+      };
+      certificatesResolvers = {
+        staging.acme = {
+          email = "x3xr6n66@notohh.dev";
+          storage = "/var/lib/traefik/acme.json";
+          caServer = "https://acme-staging-v02.api.letsencrypt.org/directory";
+          dnsChallenge = {
+            provider = "cloudflare";
+            resolvers = ["1.1.1.1:53" "1.0.0.1:53"];
+            delayBeforeCheck = "0";
+          };
+        };
+        production.acme = {
+          email = "x3xr6n66@notohh.dev";
+          storage = "/var/lib/traefik/acme.json";
+          caServer = "https://acme-v02.api.letsencrypt.org/directory";
+          dnsChallenge = {
+            provider = "cloudflare";
+            resolvers = ["1.1.1.1:53" "1.0.0.1:53"];
+            delayBeforeCheck = "0";
+          };
+        };
+      };
+    };
+  };
+}
