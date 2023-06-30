47cf5f068c9ac377a406f1bb04825efb4109c511
notohh <github@notohh.dev>
2023-06-30 16:59:27 -0400

    yuki: init minecraft

---
+++ hosts/yuki/services/minecraft.nix
@@ -0,0 +1,13 @@
+{...}: {
+  networking.firewall.allowedTCPPorts = [25565];
+  virtualisation.oci-containers.containers.minecraft = {
+    image = "marctv/minecraft-papermc-server:latest";
+    ports = ["25565:25565"];
+    environment = {
+      MEMORYSIZE = "6G";
+    };
+    volumes = [
+      "/home/notoh/docker/mcserver:/data:rw"
+    ];
+  };
+}
