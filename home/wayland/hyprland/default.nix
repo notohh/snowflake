e76f2b53cecaa0733f13192e69bfd01e3ae1494f
notohh <github@notohh.dev>
2023-07-21 09:04:06 -0400

    hyprland: config with upstream hm

--- home/wayland/hyprland/default.nix
+++ home/wayland/hyprland/default.nix
@@ -1,18 +1,11 @@
 {...}: {
-  imports = [./config.nix];
-
+  imports = [
+    ./settings.nix
+  ];
   wayland.windowManager.hyprland = {
     enable = true;
-    nvidiaPatches = true;
-  };
-
-  services.wlsunset = {
-    enable = false; #not working on my nvidia card currently
-    latitude = "40.712776";
-    longitude = "-74.005974";
-    temperature = {
-      day = 6200;
-      night = 3750;
-    };
+    enableNvidiaPatches = true;
+    systemdIntegration = true;
+    xwayland.enable = true;
   };
 }
