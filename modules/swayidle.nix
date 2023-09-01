81f43c4488883e4cfe948567dccb80c022a3d60d
notohh <github@notohh.dev>
2023-09-01 14:25:37 -0400

    swayidle: edit timing, change picture

--- modules/swayidle.nix
+++ modules/swayidle.nix
@@ -3,10 +3,10 @@ _: {
     executable = true;
     text = ''
       swayidle -w \
-         timeout 600 'swaylock -f --image ~/Pictures/wallpapers/anny.jpg --clock --grace 5 --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b' \
-         timeout 800 'hyprctl dispatch dpms off' \
+         timeout 500 'swaylock -f --image ~/Pictures/wallpapers/mumei.png --clock --grace 5 --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b' \
+         timeout 600 'hyprctl dispatch dpms off' \
              resume 'hyprctl dispatch dpms on' \
-         before-sleep 'swaylock -f --image ~/Picture/wallpapers/anny.jpg --clock --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b'
+         before-sleep 'swaylock -f --image ~/Picture/wallpapers/mumei.png --clock --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b'
     '';
   };
 }
