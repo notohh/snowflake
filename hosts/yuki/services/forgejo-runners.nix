0e29454b002cfb48e635de28caf8541c9227d98b
notohh <github@notohh.dev>
2023-10-07 19:23:31 -0400

    forgejo-runner: rename instance, rotate key and use sops

--- hosts/yuki/services/forgejo-runners.nix
+++ hosts/yuki/services/forgejo-runners.nix
@@ -6,7 +6,7 @@
   sops.secrets.forgejo-runner-token = {};
   services.gitea-actions-runner = {
     package = pkgs.forgejo-actions-runner;
-    instances.main = {
+    instances.snowflake = {
       settings = {
         container = {
           network = "host";
@@ -14,8 +14,8 @@
       };
       enable = true;
       name = config.networking.hostName;
+      token = config.sops.secrets.forgejo-runner-token.path;
       url = "https://git.flake.sh";
-      token = "gdeEbeUTifa1nK7EfRgBmvm6XRdQE1zZzAatBRSC";
       labels = [
         "debian-latest:docker://node:18-bullseye"
         "ubuntu-latest:docker://node:18-bullseye"
