0c89612491433db7ff000bc1f12a5492a97144f0
notohh <github@notohh.dev>
2023-10-08 00:19:33 -0400

    forgejo-runner: init basegbot runner

--- hosts/yuki/services/forgejo-runners.nix
+++ hosts/yuki/services/forgejo-runners.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   sops.secrets.forgejo-runner-token = {};
+  sops.secrets.basegbot-runner-token = {};
   services.gitea-actions-runner = {
     package = pkgs.forgejo-actions-runner;
     instances.snowflake = {
@@ -15,6 +16,29 @@
       enable = true;
       name = config.networking.hostName;
       token = config.sops.secrets.forgejo-runner-token.path;
+      url = "https://git.flake.sh";
+      labels = [
+        "debian-latest:docker://node:18-bullseye"
+        "ubuntu-latest:docker://node:18-bullseye"
+        #"native:host"
+      ];
+      hostPackages = with pkgs; [
+        bash
+        curl
+        coreutils
+        wget
+        gitMinimal
+      ];
+    };
+    instances.basegbot = {
+      settings = {
+        container = {
+          network = "host";
+        };
+      };
+      enable = true;
+      name = config.networking.hostName;
+      token = config.sops.secrets.basegbot-runner-token.path;
       url = "https://git.flake.sh";
       labels = [
         "debian-latest:docker://node:18-bullseye"
