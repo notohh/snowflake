f6835e00ac067267dd01851b9439e14ad78275a9
notohh <github@notohh.dev>
2023-10-08 14:27:31 -0400

    swayidle: reload ironbar after dpms off

--- modules/swayidle.nix
+++ modules/swayidle.nix
@@ -6,7 +6,8 @@ _: {
          timeout 500 'swaylock -f --image ~/Pictures/wallpapers/mumei.png --clock --grace 5 --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b' \
          timeout 600 'hyprctl dispatch dpms off' \
              resume 'hyprctl dispatch dpms on' \
-         before-sleep 'swaylock -f --image ~/Picture/wallpapers/mumei.png --clock --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b'
+         before-sleep 'swaylock -f --image ~/Picture/wallpapers/mumei.png --clock --ring-color f5c2e7 --inside-color 1e1e2e --line-color 11111b' \
+         after-resume 'ironbar reload'
     '';
   };
 }
