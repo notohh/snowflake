005f45bfab21a7c463847ac4d10e172387836700
notohh <github@notohh.dev>
2023-10-07 21:59:24 -0400

    redis: fix binding

--- hosts/arashi/services/redis.nix
+++ hosts/arashi/services/redis.nix
@@ -1,10 +1,11 @@
-{...}: {
+{pkgs, ...}: {
+  environment.systemPackages = [pkgs.redis-dump];
   services.redis.servers = {
     forgejo = {
       enable = true;
       openFirewall = true;
       port = 6379;
-      bind = "100.94.214.100";
+      bind = null;
       databases = 16;
       logLevel = "debug";
       requirePass = "forgejo";
