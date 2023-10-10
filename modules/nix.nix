20949f330006d1ce1151c01f5594952a76fa36d6
notohh <github@notohh.dev>
2023-10-10 01:13:35 -0400

    nix: add prismlauncher overlay

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,8 +1,9 @@
-_: {
+{inputs, ...}: {
   nixpkgs = {
     config = {
       allowUnfree = true;
     };
+    overlays = [inputs.prismlauncher.overlays.default];
   };
 
   nh = {
@@ -27,11 +28,13 @@ _: {
         "https://hyprland.cachix.org"
         "https://cache.nixos.org"
         "https://jakestanger.cachix.org"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "jakestanger.cachix.org-1:VWJE7AWNe5/KOEvCQRxoE8UsI2Xs2nHULJ7TEjYm7mM="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
