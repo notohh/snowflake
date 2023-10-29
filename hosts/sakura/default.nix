bbea2901e46b11d1b6d7559b9de30f5dd8dd8c37
notohh <github@notohh.dev>
2023-10-29 05:53:24 -0400

    chore: remove cowsay

--- hosts/sakura/default.nix
+++ hosts/sakura/default.nix
@@ -31,6 +31,4 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMd8j1+fC/ng7l17rsxugVtlhurUe1ICizwA9lQkSuNY forgejo"
   ];
-
-  environment.systemPackages = [pkgs.cowsay];
 }
