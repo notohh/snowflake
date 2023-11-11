11fd6aab07f93e136162b294729aa8a5cb67a326
notohh <github@notohh.dev>
2023-11-10 19:15:41 -0500

    homarr: bump version

--- hosts/yuki/services/homarr.nix
+++ hosts/yuki/services/homarr.nix
@@ -1,12 +1,13 @@
 _: {
   virtualisation.oci-containers.containers.homarr = {
-    image = "ghcr.io/ajnart/homarr:0.13.4";
+    image = "ghcr.io/ajnart/homarr:0.14.0";
     ports = [
       "7575:7575"
     ];
     volumes = [
       "/home/notohh/docker/homarr/configs:/app/data/configs"
       "/home/notohh/docker/homarr/icons:/app/public/icons"
+      "/home/notohh/docker/homarr/data:/data"
     ];
   };
 }
