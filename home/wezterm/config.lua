c5cd1b85bbe16af69a5d2c4279f724f9ce2cf224
notohh <github@notohh.dev>
2023-11-15 02:01:25 -0500

    wezterm: fmt

--- home/wezterm/config.lua
+++ home/wezterm/config.lua
@@ -15,5 +15,5 @@ return {
     enable_tab_bar = false,
     color_scheme = 'Catppuccin Mocha',
 
-    window_padding = {top = 0, bottom = 0, left = 0, right = 0}
+    window_padding = { top = 0, bottom = 0, left = 0, right = 0 }
 }
