5fe1ac2b528c46ade389750ccece477b123b82f7
notohh <github@notohh.dev>
2023-11-15 05:46:47 -0500

    nix: init anyrun cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -30,6 +30,7 @@
         "https://jakestanger.cachix.org"
         "https://cache.garnix.io"
         "https://nix-gaming.cachix.org"
+        "https://anyrun.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
@@ -37,6 +38,7 @@
         "jakestanger.cachix.org-1:VWJE7AWNe5/KOEvCQRxoE8UsI2Xs2nHULJ7TEjYm7mM="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
   };
