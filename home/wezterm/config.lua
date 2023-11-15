0e61d06ae1c431c8a9b59f799c29c3bef7ccf5eb
notohh <github@notohh.dev>
2023-11-15 05:47:06 -0500

    wezterm: switch theme and font

--- home/wezterm/config.lua
+++ home/wezterm/config.lua
@@ -8,12 +8,12 @@ return {
         name = "NVIDIA GeForce GTX 1070 Ti"
     },
     enable_wayland = false,
-    font = wezterm.font 'Comic Code Ligatures',
+    font = wezterm.font 'Monaspace Krypton',
     font_size = 12.0,
     window_background_opacity = 0.6,
     text_background_opacity = 1.0,
     enable_tab_bar = false,
-    color_scheme = 'Catppuccin Mocha',
+    color_scheme = 'tokyonight',
 
     window_padding = { top = 0, bottom = 0, left = 0, right = 0 }
 }
