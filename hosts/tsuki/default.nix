80fb4c5a3c0967407c7425f1b6877880e6cb1175
notohh <github@notohh.dev>
2023-12-09 01:41:28 -0500

    tsuki: rollback nvidia drivers due to flickering

--- hosts/tsuki/default.nix
+++ hosts/tsuki/default.nix
@@ -92,7 +92,7 @@
     nvidia = {
       powerManagement.enable = true;
       modesetting.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
     };
     opengl = {
       enable = true;
