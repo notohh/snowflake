1c0a2759bacf50ca2773055c75ee9df27d3ea786
notohh <github@notohh.dev>
2023-12-15 10:06:50 -0500

    bat: remove theme

--- home/bat/default.nix
+++ home/bat/default.nix
@@ -1,17 +1,5 @@
-{pkgs, ...}: {
+_: {
   programs.bat = {
     enable = true;
-    config = {
-      theme = "catppuccin-mocha";
-    };
-    themes = {
-      catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-        }
-        + "/Catppuccin-mocha.tmTheme");
-    };
   };
 }
