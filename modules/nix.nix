395f406973120b27d5b1ef357601397564665876
notohh <github@notohh.dev>
2023-12-23 16:29:10 -0500

    nix: allow electron-25.9.0

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,7 +1,12 @@
-{...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   nixpkgs = {
     config = {
       allowUnfree = true;
+      permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
     };
   };
 
