7bab6e4e48e0662aac020d6dfecac779b04d50c1
notohh <github@notohh.dev>
2023-12-29 12:27:43 -0500

    tsuki: init ssh config & mimetypes

--- hosts/tsuki/home.nix
+++ hosts/tsuki/home.nix
@@ -58,7 +58,82 @@
       (libsForQt5.callPackage ../../pkgs/chatterino7 {})
       (callPackage ../../pkgs/jellyfin-rpc {})
     ];
-
     stateVersion = "23.05";
+  };
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      Host sakura
+        HostName 100.121.201.47
+        User notoh
+        IdentityFile ~/.ssh/sakura
+      Host kariru
+        HostName 100.126.229.95
+        User notoh
+        IdentityFile ~/.ssh/kariru
+      Host yuki
+        HostName 100.108.113.89
+        User notoh
+        IdentityFile ~/.ssh/yuki
+      Host arashi
+        HostName 100.94.214.100
+        User notoh
+        IdentityFile ~/.ssh/arashi
+      Host sora
+        HostName 100.104.42.96
+        User notoh
+        IdentityFile ~/.ssh/sora
+      Host daphbot
+        Hostname 100.109.118.139
+        User root
+        IdentityFile ~/.ssh/daphbot
+      Host tsuru
+        HostName 100.82.146.40
+        User notoh
+        IdentityFile ~/.ssh/tsuru
+      Host haru
+        HostName 100.73.192.45
+        User notoh
+        IdentityFile ~/.ssh/haru
+      Host basegbot
+        HostName 100.83.81.116
+        User basegbot
+        IdentityFile ~/.ssh/basegbot
+      Host pihole
+        HostName 192.168.1.221
+        User notoh
+        IdentityFile ~/.ssh/pihole
+      Host rpi4
+        HostName 100.92.145.147
+        User notoh
+        IdentityFile ~/.ssh/rpi4
+      Host pve
+        Hostname 100.115.234.69
+        User root
+      Host truenas
+        Hostname 192.168.1.199
+        User root
+        IdentityFile ~/.ssh/truenas
+    '';
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/discord-409416265891971072" = ["discord-409416265891971072.desktop"];
+      "x-scheme-handler/discord-402572971681644545" = ["discord-402572971681644545.desktop"];
+      "x-scheme-handler/discord-696343075731144724" = ["discord-696343075731144724.desktop"];
+      "x-scheme-handler/http" = ["floorp.desktop"];
+      "x-scheme-handler/https" = ["floorp.desktop"];
+      "x-scheme-handler/chrome" = ["floorp.desktop"];
+      "text/html" = ["floorp.desktop"];
+      "application/x-extension-htm" = ["floorp.desktop"];
+      "application/x-extension-html" = ["floorp.desktop"];
+      "application/x-extension-shtml" = ["floorp.desktop"];
+      "application/xhtml+xml" = ["floorp.desktop"];
+      "application/x-extension-xhtml" = ["floorp.desktop"];
+      "application/x-extension-xht" = ["floorp.desktop"];
+    };
   };
 }
