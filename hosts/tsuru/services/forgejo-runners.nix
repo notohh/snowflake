9cc7a1166370484462e08a17631c9360af599800
notohh <github@notohh.dev>
2023-12-30 20:47:28 -0500

    tsuru: add nixgarden runner

--- hosts/tsuru/services/forgejo-runners.nix
+++ hosts/tsuru/services/forgejo-runners.nix
@@ -53,5 +53,30 @@
         gitMinimal
       ];
     };
+    instances.nixgarden = {
+      settings = {
+        container = {
+          network = "host";
+        };
+      };
+      enable = true;
+      name = config.networking.hostName;
+      token = "ufM3x5MANdcIg9qb7LTJrC7dAi9Kw6DmVXDDX3HL";
+      url = "https://git.flake.sh";
+      labels = [
+        "debian-latest:docker://node:18-bullseye"
+        "ubuntu-latest:docker://node:18-bullseye"
+        #"native:host"
+      ];
+      hostPackages = with pkgs; [
+        bash
+        curl
+        coreutils
+        wget
+        gitMinimal
+        python3
+        python311Packages.pip
+      ];
+    };
   };
 }
