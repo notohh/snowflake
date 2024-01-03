19423d7465448d97de03e3a20f4e307cb77a51ab
notohh <github@notohh.dev>
2024-01-03 13:41:52 -0500

    wayland: add wlsunset

---
+++ home/wayland/wlsunset.nix
@@ -0,0 +1,11 @@
+_: {
+  services.wlsunset = {
+    enable = false;
+    latitude = "40.712776";
+    longitude = "-74.005974";
+    temperature = {
+      day = 6500;
+      night = 3200;
+    };
+  };
+}
