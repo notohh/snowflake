0e822a8c9c8ad95b4a065b29d2ee68671968e609
notohh <github@notohh.dev>
2024-01-03 19:19:40 -0500

    pkgs: add overlays.nix

---
+++ pkgs/overlays.nix
@@ -0,0 +1,23 @@
+{lib, ...}: {
+  nixpkgs = {
+    overlays = [
+      (final: prev: {
+        spotify-player = prev.spotify-player.overrideAttrs (old: rec {
+          pname = "spotify-player";
+          version = "0.16.3";
+          src = prev.fetchFromGitHub {
+            owner = "aome510";
+            repo = pname;
+            rev = "refs/tags/v${version}";
+            hash = "sha256-8naLLHAVGB8ow88XjU3BpnNzY3SFC2F5uYin67hMc0E=";
+          };
+          cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
+            name = "spotify-player.tar.gz";
+            inherit src;
+            outputHash = "sha256-KiqQH356tOEz+Hjhh5XZa3/R+Jw/dH9Ys4KPyZZSTec=";
+          });
+        });
+      })
+    ];
+  };
+}
