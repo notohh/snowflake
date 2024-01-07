0e68e5ac9cde95a5005a3d6563b336b8fc7f436f
notohh <github@notohh.dev>
2024-01-07 17:50:16 -0500

    home: init firefox

---
+++ home/firefox/default.nix
@@ -0,0 +1,107 @@
+{pkgs, ...}: {
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisablePocket = true;
+      DisplayBookmarksToolbar = true;
+      DontCheckDefaultBrowser = true;
+      OfferToSaveLogins = false;
+      NoDefaultBookmarks = true;
+      PasswordManagerEnable = false;
+      DNSOverHTTPS = {
+        Enabled = true;
+      };
+    };
+    profiles.notohh = {
+      id = 0;
+      isDefault = true;
+      name = "notohh";
+      settings = {
+        "browser.search.separatePrivateDefault" = false;
+        "browser.search.separatePrivateDefault.ui.enabled" = true;
+        "browser.startup.homepage" = "http://dashboard.internal.flake.sh/";
+        "network.trr.mode" = 2;
+      };
+      extraConfig = builtins.readFile ./user.js/betterfox.js;
+      search = {
+        default = "SearXNG";
+        engines = {
+          "SearXNG" = {
+            urls = [
+              {
+                template = "http://100.121.201.47:8100";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            definedAliases = ["@sx"];
+          };
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
+          "Bing".metaData.hidden = true;
+          "Google".metaData.hidden = true;
+          "Amazon.com".metaData.hidden = true;
+          "eBay".metaData.hidden = true;
+          "DuckDuckGo".metaData.hidden = true;
+        };
+      };
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        # security
+        bitwarden
+        ublock-origin
+        canvasblocker
+
+        #leisure
+        darkreader
+        mal-sync
+
+        #utility
+        new-tab-override
+        sponsorblock
+        return-youtube-dislikes
+
+        #misc
+        tokyo-night-v2
+      ];
+    };
+  };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "x-scheme-handler/discord-409416265891971072" = ["discord-409416265891971072.desktop"];
+      "x-scheme-handler/discord-402572971681644545" = ["discord-402572971681644545.desktop"];
+      "x-scheme-handler/discord-696343075731144724" = ["discord-696343075731144724.desktop"];
+      "x-scheme-handler/http" = ["firefox.desktop"];
+      "x-scheme-handler/https" = ["firefox.desktop"];
+      "x-scheme-handler/chrome" = ["firefox.desktop"];
+      "text/html" = ["firefox.desktop"];
+      "application/x-extension-htm" = ["firefox.desktop"];
+      "application/x-extension-html" = ["firefox.desktop"];
+      "application/x-extension-shtml" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
+      "application/x-extension-xhtml" = ["firefox.desktop"];
+      "application/x-extension-xht" = ["firefox.desktop"];
+    };
+  };
+}
