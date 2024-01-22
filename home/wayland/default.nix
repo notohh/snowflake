49f9b1504ae8261806b2c06e204847c5979e8fd7
notohh <github@notohh.dev>
2024-01-22 03:48:37 -0500

    wayland: add greetd

--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -15,7 +15,7 @@
   };
 in {
   imports = [
-    ../../modules/greetd.nix
+    ./greetd.nix
   ];
 
   environment.systemPackages = with pkgs; [
---
+++ home/wayland/greetd.nix
@@ -0,0 +1,12 @@
+_: {
+  services.greetd = {
+    enable = true;
+    settings = rec {
+      initial_session = {
+        command = "Hyprland";
+        user = "notoh";
+      };
+      default_session = initial_session;
+    };
+  };
+}
