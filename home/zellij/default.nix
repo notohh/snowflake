6efe8282ce337f9089e9f5e9f6ea684c1800e048
notohh <github@notohh.dev>
2024-01-22 08:08:12 -0500

    zellij: fix theme

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -10,20 +10,7 @@
       simplified_ui = false;
       default_layout = "default";
       ui.pane_frames.rounded_corners = true;
-      theme = "tokyonight";
-      themes.catppuccin-mocha = {
-        bg = "#585b70";
-        fg = "#cdd6f4";
-        red = "#f38ba8";
-        green = "#a6e3a1";
-        blue = "#89b4fa";
-        yellow = "#f9e2af";
-        magenta = "#f5c2e7";
-        orange = "#fab387";
-        cyan = "#89dceb";
-        black = "#181825";
-        white = "#cdd6f4";
-      };
+      theme = "tokyo-night-storm";
     };
   };
 }
