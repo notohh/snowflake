8eb18d654575a51bbca9104cc2de8b12d2774976
notohh <github@notohh.dev>
2024-01-27 19:14:52 -0500

    default: remove dataDir

--- hosts/kariru/services/default.nix
+++ hosts/kariru/services/default.nix
@@ -9,13 +9,11 @@
   services.radarr = {
     enable = true;
     openFirewall = true;
-    dataDir = "/var/lib/radarr";
   };
 
   services.sonarr = {
     enable = true;
     openFirewall = true;
-    dataDir = "/var/lib/sonarr";
   };
 
   services.prowlarr = {
