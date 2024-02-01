44d1c2302b22dd2ee83f36f7979331434db56ffb
notohh <github@notohh.dev>
2024-02-01 07:39:57 -0500

    minio: listen on all hosts

--- hosts/kaze/services/minio.nix
+++ hosts/kaze/services/minio.nix
@@ -18,8 +18,8 @@
   services.minio = {
     enable = true;
     region = "us-east-1";
-    consoleAddress = "100.69.79.81:9006";
-    listenAddress = "100.69.79.81:9005";
+    consoleAddress = "0.0.0.0:9006";
+    listenAddress = "0.0.0.0:9005";
     rootCredentialsFile = config.sops.secrets.minio.path;
     dataDir = ["/var/lib/slab/minio/data"];
     configDir = "/var/lib/slab/minio/config";
