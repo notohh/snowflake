069ad76e60c98ef1ba310f59d257360ffb4ecf72
notohh <github@notohh.dev>
2024-02-02 12:18:25 -0500

    vaultwarden: init SMTP
    vaultwarden: rename secret
    
    sops: update secrets

--- hosts/sakura/services/vaultwarden.nix
+++ hosts/sakura/services/vaultwarden.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
+  sops.secrets.vaultwarden-env = {};
   services.vaultwarden = {
     enable = true;
     package = pkgs.vaultwarden-postgresql;
@@ -11,6 +16,15 @@
       ROCKET_ADDRESS = "0.0.0.0";
       ROCKET_PORT = 8222;
       ROCKET_LOG = "critical";
+      # mailer
+      SMTP_HOST = "mail.smtp2go.com";
+      SMTP_PORT = 587;
+      SMTP_SECURITY = "starttls";
+      SMTP_FROM = "vault@flake.sh";
+      SMTP_FROM_NAME = "Vaultwarden";
+      SMTP_USERNAME = "vaultwarden-mailer";
+      SMTP_TIMEOUT = 15;
     };
+    environmentFile = config.sops.secrets.vaultwarden-env.path;
   };
 }
