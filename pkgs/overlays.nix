d8a6c6e1e56f349596d173c1817df43606fbb2ca
notohh <github@notohh.dev>
2024-03-07 03:55:02 -0500

    overlays: bump spotify-player

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -7,17 +7,17 @@
       (final: prev: {
         spotify-player = prev.spotify-player.overrideAttrs (old: rec {
           pname = "spotify-player";
-          version = "0.16.3";
+          version = "0.17.0";
           src = prev.fetchFromGitHub {
             owner = "aome510";
             repo = pname;
             rev = "refs/tags/v${version}";
-            hash = "sha256-8naLLHAVGB8ow88XjU3BpnNzY3SFC2F5uYin67hMc0E=";
+            hash = "sha256-fGDIlkTaRg+J6YcP9iBcJFuYm9F0UOA+v/26hhdg9/o=";
           };
           cargoDeps = old.cargoDeps.overrideAttrs (lib.const {
             name = "spotify-player.tar.gz";
             inherit src;
-            outputHash = "sha256-KiqQH356tOEz+Hjhh5XZa3/R+Jw/dH9Ys4KPyZZSTec=";
+            outputHash = "sha256-6AdIU4Q5nxsZW9cRoRI/Zn7EQ3ZJ/EYSFfXIeatT8sY=";
           });
         });
       })
