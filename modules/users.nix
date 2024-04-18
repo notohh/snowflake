43951e41446585e96e16f253f570bd8c6633aafc
notohh <github@notohh.dev>
2024-04-17 20:29:45 -0400

    users: add greeter user

--- modules/users.nix
+++ modules/users.nix
@@ -1,6 +1,10 @@
 {pkgs, ...}: {
   users = {
     defaultUserShell = pkgs.nushell;
+    extraUsers.greeter = {
+      home = "/tmp/greeter-home";
+      createHome = true;
+    };
     users.notoh = {
       isNormalUser = true;
       uid = 1000;
