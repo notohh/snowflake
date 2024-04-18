81f693ce21464bc21564eea326f0b06cfe843465
notohh <github@notohh.dev>
2024-04-17 20:46:36 -0400

    users: remove greeter user

--- modules/users.nix
+++ modules/users.nix
@@ -1,10 +1,6 @@
 {pkgs, ...}: {
   users = {
     defaultUserShell = pkgs.nushell;
-    extraUsers.greeter = {
-      home = "/tmp/greeter-home";
-      createHome = true;
-    };
     users.notoh = {
       isNormalUser = true;
       uid = 1000;
