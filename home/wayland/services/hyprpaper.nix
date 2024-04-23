41a13caff4e303449d4518d471abe362d02e53ae
notohh <github@notohh.dev>
2024-04-23 14:52:47 -0400

    hyprpaper: update wallpapers

--- home/wayland/services/hyprpaper.nix
+++ home/wayland/services/hyprpaper.nix
@@ -5,14 +5,14 @@
     ipc = true;
     splash = false;
     preloads = [
-      "~/Pictures/wallpapers/sparkle.png"
-      "~/Pictures/wallpapers/sparkle2.png"
+      "~/Pictures/wallpapers/sparkle3.png"
+      "~/Pictures/wallpapers/sparkle4.png"
     ];
     wallpapers = [
-      "HDMI-A-1,~/Pictures/wallpapers/sparkle.png"
-      "DP-1,~/Pictures/wallpapers/sparkle.png"
-      "DP-2,~/Pictures/wallpapers/sparkle2.png"
-      "DP-3,~/Pictures/wallpapers/sparkle2.png"
+      "HDMI-A-1,~/Pictures/wallpapers/sparkle3.png"
+      "DP-1,~/Pictures/wallpapers/sparkle3.png"
+      "DP-2,~/Pictures/wallpapers/sparkle4.png"
+      "DP-3,~/Pictures/wallpapers/sparkle4.png"
     ];
   };
 }
