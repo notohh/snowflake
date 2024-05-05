c509c8b4cc28e8b3e4de53154de762fe8eba6d46
notohh <github@notohh.dev>
2024-05-04 22:05:38 -0400

    nix: set nixos cache prio

--- modules/nix.nix
+++ modules/nix.nix
@@ -37,21 +37,22 @@
       auto-optimise-store = true;
       builders-use-substitutes = true;
       keep-outputs = true;
+      keep-derivations = true;
       allowed-users = ["@wheel"];
       trusted-users = ["root" "@wheel"];
       substituters = [
+        "https://cache.nixos.org?priority=10"
         "https://cache.flake.sh/kyasshu"
         "https://hyprland.cachix.org"
-        "https://cache.nixos.org"
         "https://nix-gaming.cachix.org"
         "https://anyrun.cachix.org"
         "https://viperml.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "kyasshu:g1heIgCgG7M4San6nRsz/omcVQ1GTc7+zKKm3L9Co7o="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
         "viperml.cachix.org-1:qZhKBMTfmcLL+OG6fj/hzsMEedgKvZVFRRAhq7j8Vh8="
