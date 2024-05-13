27db5847fb54d0ed91c446092539a0d89952bf38
notohh <github@notohh.dev>
2024-05-13 11:15:13 -0400

    flake.nix: remove hyprcursor

--- flake.nix
+++ flake.nix
@@ -60,7 +60,6 @@
     hypridle.url = "github:hyprwm/hypridle";
     hyprlock.url = "github:hyprwm/hyprlock";
     hyprpicker.url = "github:hyprwm/hyprpicker";
-    hyprcursor.url = "github:hyprwm/hyprcursor";
     hyprpaper.url = "github:hyprwm/hyprpaper";
     xdg-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
     hyprland-plugins = {
