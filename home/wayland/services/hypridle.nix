d7e3f642ae7695580018179686be2e5b9cb01307
notohh <github@notohh.dev>
2024-05-13 11:13:29 -0400

    hypr*: switch to upstream hm modules

--- home/wayland/services/hypridle.nix
+++ home/wayland/services/hypridle.nix
@@ -1,25 +1,27 @@
 {
+  inputs,
   pkgs,
   lib,
   config,
   ...
 }: {
+  imports = [inputs.hypridle.homeManagerModules.default];
+  disabledModules = ["${inputs.home-manager}/modules/services/hypridle.nix"];
+
   services.hypridle = {
     enable = true;
-    settings = {
-      ignoreDbusInhibit = false;
-      lockCmd = lib.getExe config.programs.hyprlock.package;
-      beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
-      listeners = [
-        {
-          timeout = 400;
-          onTimeout = "${lib.getExe config.programs.hyprlock.package}";
-        }
-        {
-          timeout = 460;
-          onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
-        }
-      ];
-    };
+    ignoreDbusInhibit = false;
+    lockCmd = lib.getExe config.programs.hyprlock.package;
+    beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
+    listeners = [
+      {
+        timeout = 400;
+        onTimeout = "${lib.getExe config.programs.hyprlock.package}";
+      }
+      {
+        timeout = 460;
+        onTimeout = "${pkgs.systemd}/bin/systemctl suspend";
+      }
+    ];
   };
 }
--- home/wayland/services/hyprpaper.nix
+++ home/wayland/services/hyprpaper.nix
@@ -1,19 +1,25 @@
-_: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.hyprpaper];
+  imports = [inputs.hyprpaper.homeManagerModules.default];
+  disabledModules = ["${inputs.home-manager}/modules/services/hyprpaper.nix"];
+
   services.hyprpaper = {
     enable = true;
-    settings = {
-      ipc = true;
-      splash = false;
-      preloads = [
-        "~/Pictures/wallpapers/sparkle3.png"
-        "~/Pictures/wallpapers/sparkle4.png"
-      ];
-      wallpapers = [
-        "HDMI-A-1,~/Pictures/wallpapers/sparkle3.png"
-        "DP-1,~/Pictures/wallpapers/sparkle4.png"
-        "DP-2,~/Pictures/wallpapers/sparkle4.png"
-        "DP-3,~/Pictures/wallpapers/sparkle3.png"
-      ];
-    };
+    ipc = true;
+    splash = false;
+    preloads = [
+      "~/Pictures/wallpapers/sparkle3.png"
+      "~/Pictures/wallpapers/sparkle4.png"
+    ];
+    wallpapers = [
+      "HDMI-A-1,~/Pictures/wallpapers/sparkle3.png"
+      "DP-1,~/Pictures/wallpapers/sparkle4.png"
+      "DP-2,~/Pictures/wallpapers/sparkle4.png"
+      "DP-3,~/Pictures/wallpapers/sparkle3.png"
+    ];
   };
 }
