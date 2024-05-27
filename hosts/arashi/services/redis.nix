d2c9754e49b398448fe4e4b999932bf07ed668e6
notohh <github@notohh.dev>
2024-05-27 10:24:01 -0400

    redis: switch to valkey

--- hosts/arashi/services/redis.nix
+++ hosts/arashi/services/redis.nix
@@ -1,52 +1,55 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.redis-dump];
-  services.redis.servers = {
-    forgejo = {
-      enable = true;
-      openFirewall = true;
-      port = 6379;
-      bind = null;
-      databases = 16;
-      logLevel = "debug";
-      requirePass = "forgejo";
-      settings = {
-        protected-mode = "no";
+  services.redis = {
+    package = pkgs.valkey;
+    servers = {
+      forgejo = {
+        enable = true;
+        openFirewall = true;
+        port = 6379;
+        bind = null;
+        databases = 16;
+        logLevel = "debug";
+        requirePass = "forgejo";
+        settings = {
+          protected-mode = "no";
+        };
       };
-    };
-    searxng = {
-      enable = true;
-      openFirewall = true;
-      port = 6380;
-      bind = null;
-      databases = 16;
-      logLevel = "debug";
-      requirePass = "searxng";
-      settings = {
-        protected-mode = "no";
+      searxng = {
+        enable = true;
+        openFirewall = true;
+        port = 6380;
+        bind = null;
+        databases = 16;
+        logLevel = "debug";
+        requirePass = "searxng";
+        settings = {
+          protected-mode = "no";
+        };
       };
-    };
-    blocky = {
-      enable = true;
-      openFirewall = true;
-      port = 6381;
-      bind = null;
-      databases = 16;
-      logLevel = "debug";
-      requirePass = "blocky";
-      settings = {
-        protected-mode = "no";
+      blocky = {
+        enable = true;
+        openFirewall = true;
+        port = 6381;
+        bind = null;
+        databases = 16;
+        logLevel = "debug";
+        requirePass = "blocky";
+        settings = {
+          protected-mode = "no";
+        };
       };
-    };
-    paperless-ngx = {
-      enable = true;
-      openFirewall = true;
-      port = 6382;
-      bind = null;
-      databases = 16;
-      logLevel = "debug";
-      requirePass = "paperless-ngx";
-      settings = {
-        protected-mode = "no";
+      paperless-ngx = {
+        enable = true;
+        openFirewall = true;
+        port = 6382;
+        bind = null;
+        databases = 16;
+        logLevel = "debug";
+        requirePass = "paperless-ngx";
+        settings = {
+          protected-mode = "no";
+        };
       };
     };
   };
