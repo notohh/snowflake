3cfa08ff3c944915962453bd9c108bfe583d8f19
notohh <github@notohh.dev>
2024-05-27 12:07:25 -0400

    wallos: bump version

--- hosts/yuki/services/wallos.nix
+++ hosts/yuki/services/wallos.nix
@@ -1,6 +1,6 @@
 _: {
   virtualisation.oci-containers.containers.wallos = {
-    image = "bellamy/wallos@sha256:142d7f1202cc0d80a02e9ae48c1a53de7cf5f448758d853a1fe8f5af732459fd"; #v 1.27.1
+    image = "bellamy/wallos@sha256:16877ace60e407ca11a5db818de6c67e6ba2fd624c68255ed500f8e9af965f93"; #v 2.0.0
     ports = ["8282:80"];
     volumes = [
       "/home/notoh/docker/wallos/db:/var/www/html/db"
