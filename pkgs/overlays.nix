4b4014b76c2bf3c133c5ec532a8db7576772d0f1
notohh <github@notohh.dev>
2024-06-09 00:01:22 -0400

    pkgs: add technorino overlay, remove c7 pkg

--- pkgs/overlays.nix
+++ pkgs/overlays.nix
@@ -1,6 +1,21 @@
 {lib, ...}: {
   nixpkgs = {
     overlays = [
+      (self: super: {
+        technorino =
+          super.chatterino2.overrideAttrs
+          (oldAttrs: {
+            nativeBuildInputs = with super; [cmake pkg-config];
+            buildInputs = with super; [qt6.wrapQtAppsHook qt6.qtbase qt6.qtsvg qt6.qtimageformats qt6.qttools qt6.qt5compat qt6.qtwayland boost openssl];
+            cmakeFlags = ["-DBUILD_WITH_QT6=ON" "-DBUILD_WITH_QTKEYCHAIN=OFF"];
+            src = super.chatterino2.src.override {
+              owner = "2547techno";
+              repo = "technorino";
+              rev = "nightly-build";
+              sha256 = "sha256-PY+K1l29wwG8ddqTxOwkbKb8CQoOBwbOeCgn0VTDptM=";
+            };
+          });
+      })
       (final: prev: {
         spotify-player = prev.spotify-player.overrideAttrs (old: rec {
           pname = "spotify-player";
