edf73c442b1afdfb2c3388e34cb8303e7592f0e4
notohh <github@notohh.dev>
2024-11-01 12:19:54 -0400

    neovim: switch to cord.nvim

--- home/programs/editors/neovim/plugins/misc/presence.nix
+++ home/programs/editors/neovim/plugins/misc/presence.nix
@@ -1,9 +1,30 @@
-_: {
-  programs.nixvim.plugins.neocord = {
-    enable = true;
-    settings = {
-      auto_update = true;
-      enable_line_number = true;
-    };
+{pkgs, ...}: {
+  programs.nixvim = {
+    extraPlugins = with pkgs.vimPlugins; [
+      cord-nvim
+    ];
+    extraConfigLua = ''
+      require('cord').setup {
+        editor = {
+          tooltip = 'Neovim'
+        },
+        display = {
+          show_time = true,
+          show_repository = true,
+          show_cursor_position = true,
+        },
+        lsp = {
+          show_problem_count = true,
+          severity = 1,
+          scope = 'workspace',
+        },
+        buttons = {
+          {
+            label = 'View Repository',
+            url = 'git',
+          },
+        },
+      }
+    '';
   };
 }
