67c4b9702f959b796caada5f31f2fb3679fe6fb4
notohh <github@notohh.dev>
2024-12-24 00:52:40 -0500

    zellij: update settings

--- home/programs/terminal/zellij/default.nix
+++ home/programs/terminal/zellij/default.nix
@@ -8,7 +8,10 @@ _: {
     settings = {
       on_force_close = "quit";
       simplified_ui = false;
+      theme = "tokyo-night";
       default_layout = "default";
+      copy_command = "wl-copy";
+      copy_clipboard = "primary";
       ui.pane_frames.rounded_corners = true;
     };
   };
