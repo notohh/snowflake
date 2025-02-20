475d7def0c95482ed5a27233b7b880d331e7520c
notohh <github@notohh.dev>
2025-02-20 16:27:45 -0500

    overlays: bump technorino

--- overlays/technorino.nix
+++ overlays/technorino.nix
@@ -4,14 +4,14 @@ _: {
       prev.chatterino2.overrideAttrs
       (old: {
         nativeBuildInputs = with prev; [cmake pkg-config];
-        buildInputs = with prev; [qt6.wrapQtAppsHook qt6.qtbase qt6.qtsvg qt6.qtimageformats qt6.qttools qt6.qt5compat qt6.qtwayland boost openssl];
+        buildInputs = with prev; [qt6.wrapQtAppsHook qt6.qtbase qt6.qtsvg qt6.qtimageformats qt6.qttools qt6.qt5compat qt6.qtwayland boost openssl libnotify];
         cmakeFlags = ["-DBUILD_WITH_QT6=ON" "-DBUILD_WITH_QTKEYCHAIN=OFF"];
         src = prev.chatterino2.src.override {
           owner = "2547techno";
           repo = "technorino";
-          rev = "24fa230d7f4c726ee2b731b8d79379eb0d220b4e";
+          rev = "8dff24df72dce406c31afafacf2103d3263093f0";
           tag = null;
-          hash = "sha256-X74Tg2muO3Wtfc5nH4QvJdOMBVK1uacU+QZMo/5Lf6E=";
+          hash = "sha256-eMstzHM27PCAjoeZwB8sO5ko8HmpTKi7UyupdQwAeSo=";
         };
       });
   };
