58417446b1cde5c6839aa7a2f6c1e902dfab9526
notohh <github@notohh.dev>
2025-05-05 03:22:34 -0400

    firefox: update mimeapps

--- home/programs/browsers/firefox/default.nix
+++ home/programs/browsers/firefox/default.nix
@@ -93,7 +93,6 @@
         darkreader
         mal-sync
         stylus
-        firefox-color
 
         #utility
         new-tab-override
@@ -103,22 +102,25 @@
       ];
     };
   };
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "x-scheme-handler/discord-409416265891971072" = ["discord-409416265891971072.desktop"];
-      "x-scheme-handler/discord-402572971681644545" = ["discord-402572971681644545.desktop"];
-      "x-scheme-handler/discord-696343075731144724" = ["discord-696343075731144724.desktop"];
-      "x-scheme-handler/http" = ["zen.desktop"];
-      "x-scheme-handler/https" = ["zen.desktop"];
-      "x-scheme-handler/chrome" = ["zen.desktop"];
-      "text/html" = ["firefox.desktop"];
-      "application/x-extension-htm" = ["zen.desktop"];
-      "application/x-extension-html" = ["zen.desktop"];
-      "application/x-extension-shtml" = ["zen.desktop"];
-      "application/xhtml+xml" = ["zen.desktop"];
-      "application/x-extension-xhtml" = ["zen.desktop"];
-      "application/x-extension-xht" = ["zen.desktop"];
+  xdg = {
+    configFile."mimeapps.list".force = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/discord-409416265891971072" = ["discord-409416265891971072.desktop"];
+        "x-scheme-handler/discord-402572971681644545" = ["discord-402572971681644545.desktop"];
+        "x-scheme-handler/discord-696343075731144724" = ["discord-696343075731144724.desktop"];
+        "x-scheme-handler/http" = ["zen.desktop"];
+        "x-scheme-handler/https" = ["zen.desktop"];
+        "x-scheme-handler/chrome" = ["zen.desktop"];
+        "text/html" = ["zen.desktop"];
+        "application/x-extension-htm" = ["zen.desktop"];
+        "application/x-extension-html" = ["zen.desktop"];
+        "application/x-extension-shtml" = ["zen.desktop"];
+        "application/xhtml+xml" = ["zen.desktop"];
+        "application/x-extension-xhtml" = ["zen.desktop"];
+        "application/x-extension-xht" = ["zen.desktop"];
+      };
     };
   };
 }
