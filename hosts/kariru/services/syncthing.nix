5be7251d91a4703f9daed872304f6762a60a042d
notohh <github@notohh.dev>
2025-05-05 03:29:35 -0400

    kariru: update syncthing address

--- hosts/kariru/services/syncthing.nix
+++ hosts/kariru/services/syncthing.nix
@@ -1,11 +1,10 @@
 _: {
-  networking.firewall.allowedTCPPorts = [8385];
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     dataDir = "/home/notoh/sync";
     configDir = "/home/notoh/.config/syncthing";
-    guiAddress = "192.168.1.54:8385";
+    guiAddress = "100.126.229.95:8386";
     user = "notoh";
     group = "users";
   };
