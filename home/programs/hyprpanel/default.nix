cd507510d55c775b63b816ccdf607ba217b1ea32
notohh <github@notohh.dev>
2025-07-01 00:24:04 -0400

    hyprpanel: switch to upstream hm module

--- home/programs/hyprpanel/default.nix
+++ home/programs/hyprpanel/default.nix
@@ -1,23 +1,9 @@
-{inputs, ...}: {
+_: {
   imports = [
-    inputs.hyprpanel.homeManagerModules.hyprpanel
-    inputs.sops-nix.homeManagerModules.sops
     ./settings.nix
   ];
 
   programs.hyprpanel = {
     enable = true;
-    hyprland.enable = false;
-    overwrite.enable = true;
-    override = {
-      "theme.bar.buttons.workspaces.hover" = "#7f849c";
-      "theme.bar.buttons.workspaces.active" = "#f5c2e7";
-      "theme.bar.buttons.workspaces.occupied" = "#f5c2e7";
-      "theme.bar.buttons.workspaces.available" = "#585b70";
-      "theme.bar.buttons.workspaces.border" = "#f9e2af";
-      "theme.bar.buttons.dashboard.icon" = "#74c7ec";
-      "theme.bar.buttons.media.icon" = "#1ED760";
-      "bar.customModules.cava.showIcon" = false;
-    };
   };
 }
