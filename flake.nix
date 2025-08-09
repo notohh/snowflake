85697df7caa9780c39d34481c14982fb76fd05f1
notohh <github@notohh.dev>
2025-08-09 05:21:30 -0400

    flake.nix: remove quickshell

--- flake.nix
+++ flake.nix
@@ -51,10 +51,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     deploy-rs.url = "github:serokell/deploy-rs";
     sops-nix.url = "github:Mic92/sops-nix";
     zen.url = "github:youwen5/zen-browser-flake";
