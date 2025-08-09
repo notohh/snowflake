2e03f90a01c3ccc78463fc222e57aba5f8c70a57
notohh <github@notohh.dev>
2025-08-09 04:02:55 -0400

    immich-backup: add second backup

--- hosts/tsuki/services/immich-backup.nix
+++ hosts/tsuki/services/immich-backup.nix
@@ -13,6 +13,7 @@
         echo $DISPLAY
         export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
         ${lib.getExe immich-cli} upload --album-name "desktop screenshots" --recursive ${homeDir}/Pictures/screenshots
+        ${lib.getExe immich-cli} upload --album-name "folderv2" --recursive ${homeDir}/Pictures/folderv2
         ${lib.getExe libnotify} --app-name "immich-backup" --urgency=low --icon=dialog-information "immich backup completed"
       '';
       serviceConfig = {
