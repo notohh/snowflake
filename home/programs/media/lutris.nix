28a2cd626f0232b690630b0ee764c4fe3ee9145a
notohh <github@notohh.dev>
2025-08-28 00:27:08 -0400

    media: add lutris with hm

---
+++ home/programs/media/lutris.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  osConfig,
+  ...
+}: {
+  programs.lutris = {
+    enable = true;
+    extraPackages = with pkgs; [
+      mangohud
+      wine-staging
+      winetricks
+      umu-launcher
+    ];
+    protonPackages = [
+      pkgs.proton-ge-bin
+    ];
+    steamPackage = osConfig.programs.steam.package;
+  };
+}
