65e32b6804cf2e5030f9a8d32c77e74bd619696d
notohh <github@notohh.dev>
2025-09-16 03:45:31 -0400

    gale: bump version

--- overlays/gale.nix
+++ overlays/gale.nix
@@ -1,14 +1,14 @@
 _: {
-  flake.overlays.gale = _final: prev: {
-    gale = prev.gale.overrideAttrs (n: _o: let
-      version = "1.9.5";
+  flake.overlays.gale = _: prev: {
+    gale = prev.gale.overrideAttrs (n: _: let
+      version = "1.9.7";
     in {
       src = prev.fetchFromGitHub {
         inherit version;
         owner = "Kesomannen";
         repo = "gale";
         rev = version;
-        hash = "sha256-LdMuAQ7eTPiEZRxxjGdycrI53JwJQ3grK5QORPhfo50=";
+        hash = "sha256-XEc8h7A1q+WfPl2HojFt2oIlAnNswq3X0o6jMZrEjCQ=";
       };
       pnpmDeps = prev.pnpm_10.fetchDeps {
         hash = "sha256-QQXP/x7AjDtUpe6h+pC6vUsIAptv1kN/1MJZjHAIdMo=";
@@ -23,7 +23,7 @@ _: {
           src
           cargoRoot
           ;
-        hash = "sha256-0wWU9Tf5NlYXrflIDghEwyjeR8j96CK0TATU1tWO418=";
+        hash = "sha256-zaTbb1+JK9mA9Tvnatw8lse5PBhKknDM48mN/sWLQ6w=";
       };
     });
   };
