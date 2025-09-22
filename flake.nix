cf570c14ab886dcc9e88e5557ea1f58bb5fefaa6
notohh <github@notohh.dev>
2025-09-22 17:56:22 -0400

    flake.nix: readd nix-index-db

--- flake.nix
+++ flake.nix
@@ -52,6 +52,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     deploy-rs.url = "github:serokell/deploy-rs";
     sops-nix.url = "github:Mic92/sops-nix";
     zen.url = "github:youwen5/zen-browser-flake";
