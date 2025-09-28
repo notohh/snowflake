9841902a9aaed743fd00cea9bb072cab605fd62c
notohh <github@notohh.dev>
2025-09-28 05:51:27 -0400

    sakura: remove clickhouse mount

--- hosts/sakura/hardware.nix
+++ hosts/sakura/hardware.nix
@@ -35,10 +35,6 @@
       device = "/dev/disk/by-uuid/db3e4722-35a6-44fb-8e4d-a75166b845cb";
       fsType = "ext4";
     };
-    "/home/notoh/rustlog/clickhouse" = {
-      device = "192.168.1.199:/mnt/Sutoreji/twitchlogs-rustlog";
-      fsType = "nfs";
-    };
     "/nas/restic" = {
       device = "192.168.1.199:/mnt/Sutoreji/nix-restic-data/sakura";
       fsType = "nfs";
