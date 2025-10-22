1f2f81c3c8cf16edf06306b1a75bcabfcbace4ab
notohh <github@notohh.dev>
2025-10-22 03:30:31 -0400

    hyprland: allow tearing for all steam apps
    hyprland: readd tearing for cs2
    
    hyprland: update other rules

--- home/wayland/hyprland/config/rules.nix
+++ home/wayland/hyprland/config/rules.nix
@@ -15,12 +15,10 @@ _: {
       "size 1200 600, title:^(satty)$"
       "idleinhibit focus, class:^(mpv)$"
       "idleinhibit always, class:^(firefox)$, title:^(.*YouTube.*|.*Twitch.*)$"
-      "idleinhibit always, class:^(OBS Studio)$"
-      "idleinhibit always, class:^(Moonlight)$"
-      "idleinhibit always, class:^(VRChat)$"
-      "idleinhibit fullscreen, class:^(firefox)$"
-      "immediate, class:^(quakelive_steam)$"
-      "size 2560 1440, class:^(quakelive_steam)$"
+      "idleinhibit always, class:^(com.obsproject.Studio)$"
+      "idleinhibit always, class:^(com.moonlight_stream.Moonlight)$"
+      "idleinhibit always, class:^(steam_app_)(.*)$"
+      "immediate, class:^(steam_app_)(.*)$"
       "immediate, class:^(cs2)$"
     ];
     layerrule = [
