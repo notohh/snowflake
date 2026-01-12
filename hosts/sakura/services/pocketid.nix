ebcca6913811bcf5894390093ca4b1ac65d23ae9
notohh <github@notohh.dev>
2026-01-12 16:03:12 -0500

    pocketid: remove deprecated values

--- hosts/sakura/services/pocketid.nix
+++ hosts/sakura/services/pocketid.nix
@@ -14,9 +14,7 @@
       ALLOW_USER_SIGNUPS = "disabled";
       UI_CONFIG_DISABLED = true;
       ALLOW_OWN_ACCOUNT_EDIT = true;
-      DB_PROVIDER = "postgres";
       DB_CONNECTION_STRING = "postgres://pocketid:pocketid@192.168.1.211:5432/pocketid";
-      KEYS_STORAGE = "database";
       SMTP_HOST = "mail.smtp2go.com";
       SMTP_PORT = 587;
       SMTP_FROM = "passport@flake.sh";
