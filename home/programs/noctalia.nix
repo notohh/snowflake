00a1d234d6c173c5ff86e161d72a22307390d77b
notohh <github@notohh.dev>
2026-02-27 06:36:41 -0500

    noctalia: update cfg

--- home/programs/noctalia.nix
+++ home/programs/noctalia.nix
@@ -43,17 +43,23 @@
               hideUnoccupied = false;
               labelMode = "none";
             }
+            {
+              id = "ActiveWindow";
+              maxWidth = 200;
+              colorizeIcons = true;
+            }
           ];
           center = [
             {
               id = "MediaMini";
               hideWhenIdle = false;
-              maxWidth = 240;
+              maxWidth = 300;
               useFixedWidth = true;
               showVisualizer = true;
-              visualizerType = "mirrored";
+              visualizerType = "wave";
               showAlbumArt = true;
               showProgressRing = true;
+              scrollingMode = "always";
             }
           ];
           right = [
@@ -80,35 +86,10 @@
       controlCenter = {
         position = "close_to_bar_button";
         diskPath = "/";
-        shortcuts = {
-          left = [
-            {
-              id = "Network";
-            }
-          ];
-          right = [
-            {
-              id = "Notifications";
-            }
-            {
-              id = "PowerProfile";
-            }
-            {
-              id = "KeepAwake";
-            }
-            {
-              id = "NightLight";
-            }
-          ];
-        };
         cards = [
           {
             enabled = true;
             id = "profile-card";
-          }
-          {
-            enabled = true;
-            id = "shortcuts-card";
           }
           {
             enabled = true;
