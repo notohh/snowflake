ca03121d69113c71931aacdfed18ceec033aa318
notohh <github@notohh.dev>
2026-02-27 07:00:16 -0500

    wayland: fix niri screenshare

--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -7,10 +7,12 @@
       wl-clipboard-rs
     ];
     sessionVariables = {
+      XDG_CURRENT_DESKTOP = "niri";
+      XDG_SESSION_DESKTOP = "niri";
+      XDG_SESSION_TYPE = "wayland";
       __GL_GSYNC_ALLOWED = "0";
       __GL_VRR_ALLOWED = "0";
       WLR_DRM_NO_ATOMIC = "1";
-      XDG_SESSION_TYPE = "wayland";
       _JAVA_AWT_WM_NONREPARENTING = "1";
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
@@ -19,7 +21,7 @@
       XCURSOR_SIZE = "24";
       NIXOS_OZONE_WL = "1";
       EDITOR = "hx";
-      TERMINAL = "wezterm";
+      TERMINAL = "ghostty";
     };
   };
 
@@ -31,7 +33,17 @@
       xdg-desktop-portal-gnome
     ];
     config = {
-      common.default = "*";
+      common = {
+        default = [ "gtk" ];
+      };
+      niri = {
+        default = [
+          "gtk"
+          "gnome"
+        ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+      };
     };
   };
 }
