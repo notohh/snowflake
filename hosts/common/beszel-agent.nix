f5166481034dad825441808efb96da6eb023aa8e
notohh <github@notohh.dev>
2026-04-06 22:56:04 -0400

    hosts: update beszel-agent

--- hosts/common/beszel-agent.nix
+++ hosts/common/beszel-agent.nix
@@ -1,12 +1,13 @@
 _: {
   sops.secrets.beszel-agent-token = {
+    owner = "beszel-agent";
   };
   services.beszel.agent = {
     enable = true;
     smartmon.enable = true;
     openFirewall = true;
     environment = {
-      DISABLE_SSH = "true";
+      DISABLE_SSH = "false";
       HUB_URL = "100.108.113.89";
       LISTEN = "0.0.0.0:45876";
       KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbMTB+RvVeTI24NvoAwNLa9DvnG6Wk9LIqCNMyEKGZg";
